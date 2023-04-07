3fba44df26d963b87007c406cfdd4c5fdf41eaf6
Leomar Duarte <leomar.duarte@MacBook-Pro-de-Leomar.local>
2023-04-07 12:48:10 -0300

    Implement view method

--- main.py
+++ main.py
@@ -6,3 +6,13 @@ clean   = ('clear','cls')[platform.system() == 'Windows']
 p_char  = 'X'
 c_char  = 'O'
 table   = ['1','2','3', '4','5','6','7','8','9']
+
+def view():
+    global table
+
+    print (" %s | %s | %s " % (table[0], table[1], table[2]))
+    print ("---+---+---")
+    print (" %s | %s | %s " % (table[3], table[4], table[5]))
+    print ("---+---+---")
+    print (" %s | %s | %s " % (table[6], table[7], table[8]))
+    print ("---+---+---")
