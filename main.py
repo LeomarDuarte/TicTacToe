c074ca4067713881346822afe4a441af169dca10
Leomar Duarte <leomar.duarte@MacBook-Pro-de-Leomar.local>
2023-04-07 12:44:34 -0300

    config imports and create variables

---
+++ main.py
@@ -0,0 +1,8 @@
+import os
+import platform
+
+turn    = 1 # 1 - Player 1 | 2 - Player 2
+clean   = ('clear','cls')[platform.system() == 'Windows']
+p_char  = 'X'
+c_char  = 'O'
+table   = ['1','2','3', '4','5','6','7','8','9']
